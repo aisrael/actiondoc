0489a3fd409d698d4e43afd16dc3040e4035cc34
Alistair Israel <aisrael@gmail.com>
2022-11-24 23:38:38 -0500

    Monkey-patch Hash instead

--- lib/generator.rb
+++ lib/generator.rb
@@ -9,25 +9,27 @@ TEMPLATES_DIR = File.expand_path('../templates', __dir__)
 
 # The documentation generator class
 class Generator
-  attr_reader :args, :options
+  attr_reader :args, :options, :padding
 
   # Initialize an instance of this `Generator` class
   def initialize(args, options)
     @args = args
     @options = options
+    @padding = options.fetch(:padding, :nice)
   end
 
   def run
     yaml = read_action_yml
     inputs = yaml.fetch(:inputs, {}).map do |k, v|
-      v.merge({ name: k.to_s })
+      { name: k.to_s }.merge(v)
     end
+    warn inputs.inspect
     action = Action.new(*(yaml.values_at(:name, :description) + [inputs]))
     render_default_template(action)
   end
 
   def read_action_yml
-    deep_symbolize_keys(YAML.safe_load(File.read('action.yml')))
+    YAML.safe_load(File.read('action.yml')).deep_symbolize_keys
   end
 
   def render_default_template(action)
@@ -36,13 +38,19 @@ class Generator
   end
 end
 
-def deep_symbolize_keys(obj)
-  case obj
-  when Hash
-    obj.transform_keys(&:to_sym).transform_values { |v| deep_symbolize_keys(v) }
-  when Array
-    obj.map { |e| deep_symbolize_keys(e) }
-  else
-    obj
+# Monkey patch Hash
+module HashExt
+  def deep_symbolize_keys
+    transform_keys(&:to_sym).transform_values do |v|
+      case v
+      when Hash
+        v.deep_symbolize_keys
+      when Array
+        v.map { |e| e.is_a?(Hash) ? e.deep_symbolize_keys : e }
+      else
+        v
+      end
+    end
   end
 end
+Hash.include HashExt
--- test/generate_test.rb
+++ test/generate_test.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+$LOAD_PATH << File.expand_path('../lib', __dir__)
+
 require 'aruba/api'
+require 'generator'
 require 'minitest/autorun'
 
 Aruba.configure do |config|
@@ -18,5 +21,20 @@ class GenerateTest < Minitest::Test
     run_command_and_stop 'ruby generate.rb --version'
 
     assert_equal('generate.rb version 0.1.0', last_command_started.output.chomp)
+  end
+end
+
+class DeepSymbolizeKeysTest < Minitest::Test
+  def test_deep_symbolize_keys
+    cases = {
+      { 'a' => 1 } => { a: 1 },
+      { b: 2 } => { b: 2 },
+      { 'c' => [3] } => { c: [3] },
+      { 'x' => { 'y' => [{ 'z' => 26 }, 27] } } => { x: { y: [{ z: 26 }, 27] } }
+    }
+
+    cases.each do |input, expected|
+      assert_equal(expected, input.deep_symbolize_keys)
+    end
   end
 end
