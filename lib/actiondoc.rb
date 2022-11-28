fda6980fc129a12963b2c0aa4ef0ec3847d1a7b6
Alistair Israel <aisrael@gmail.com>
2022-11-28 23:34:03 +0800

    Support custom (ERB) templates (#1)
    * First cut of custom (ERB) template
    
    * Add language highlighting hints
    
    * Try auto-generating the README
    
    * rubocop -A
    
    * Cucumber
    
    * Shellcheck
    
    * Fixing the e2e test
    
    * This, too
    
    * Let's try updating the README
    
    * [create-pull-request] automated change
    
    * Get those to pass
    
    Co-authored-by: aisrael <aisrael@users.noreply.github.com>

--- lib/actiondoc.rb
+++ lib/actiondoc.rb
@@ -9,7 +9,19 @@ module ActionDoc
   class Error < StandardError; end
 
   class << self
+    # The main CLI entrypoint
     def run
+      options = parse_options
+
+      if options[:version]
+        display_version
+      else
+        ActionDoc::Generator.new(ARGV, options).run
+      end
+    end
+
+    # Builds the OptionParser and parses the options
+    def parse_options
       options = {}
 
       optparser = build_optparser(options)
@@ -21,17 +33,17 @@ module ActionDoc
         exit
       end
 
-      if options[:version]
-        display_version
-      else
-        ActionDoc::Generator.new(ARGV, options).run
-      end
+      options
     end
 
+    # Builds and returns the OptionParser
     def build_optparser(options)
       OptionParser.new do |opts|
         opts.banner = 'Usage: actiondoc [options]'
 
+        opts.on('--template=TEMPLATE_FILENAME', '-t=TEMPLATE_FILENAME', 'The template to use') do |template_filename|
+          options[:template] = template_filename
+        end
         opts.on('--version', 'Show the version') do
           options[:version] = true
         end
